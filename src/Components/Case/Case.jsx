87fce1f1447cab4247d15d0aabf58ec49f5eb95e
Bohdan Nahorniak <triggerok1488@gmail.com>
2023-12-01 21:31:02 +0200

    fixed focus bug

--- src/Components/Case/Case.jsx
+++ src/Components/Case/Case.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './Case.css'
 
 const Case = ({ title, id, handleDelete, handleUpdate }) => {
-
 	//State for toggle buttons update and save
 	const [isReadOnly, setIsReadOnly] = useState(true)
 
@@ -12,27 +11,47 @@ const Case = ({ title, id, handleDelete, handleUpdate }) => {
 	return (
 		<div className='task'>
 			<div className='content'>
-				<input
-					type='text'
-					className='text'
-					value={titleInput}
-					readOnly={isReadOnly}
-					onChange={(e) => setTitleInput(e.target.value)}
-					ref={(ref) => ref && ref.focus()}
-					onFocus={(e) =>
-						e.currentTarget.setSelectionRange(
-							e.currentTarget.value.length,
-							e.currentTarget.value.length
-						)
-					}
-				/>
+				{isReadOnly ? (
+					<input
+						type='text'
+						className='text'
+						value={titleInput}
+						readOnly
+					/>
+				) : (
+					<input
+						type='text'
+						className='text'
+						value={titleInput}
+						onChange={(e) => setTitleInput(e.target.value)}
+						ref={(ref) => ref && ref.focus()}
+						onFocus={(e) =>
+							e.currentTarget.setSelectionRange(
+								e.currentTarget.value.length,
+								e.currentTarget.value.length
+							)
+						}
+					/>
+				)}
 			</div>
 			<div className='actions'>
-				{isReadOnly 
-				//Toggle button for ability to update task name
-				? (<button className='edit' onClick={() => setIsReadOnly(false)}>Update</button>) 
-				//Button for saving updated task name and toggle to prev button with read only mode
-				: (<button className='edit'	onClick={() => {handleUpdate(id, titleInput); setIsReadOnly(true)}}>Save</button>)}
+				{isReadOnly ? (
+					//Toggle button for ability to update task name
+					<button className='edit' onClick={() => setIsReadOnly(false)}>
+						Update
+					</button>
+				) : (
+					//Button for saving updated task name and toggle to prev button with read only mode
+					<button
+						className='edit'
+						onClick={() => {
+							handleUpdate(id, titleInput)
+							setIsReadOnly(true)
+						}}
+					>
+						Save
+					</button>
+				)}
 				<button className='delete' onClick={() => handleDelete(id)}>
 					Delete
 				</button>
